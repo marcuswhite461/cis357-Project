f9f04910e20c0c59a1f694933e93fecbcee8bf94
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2023-12-08 20:09:09 -0500

    Basic maps fragment

--- app/src/main/java/com/example/blank_app/MapActivity.kt
+++ app/src/main/java/com/example/blank_app/MapActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mGoogleMap:GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
@@ -20,8 +26,15 @@ class MapActivity : AppCompatActivity() {
             finish()
         }
 
+        //map
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
 
+    }
 
+    //on map ready
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
     }
 }
