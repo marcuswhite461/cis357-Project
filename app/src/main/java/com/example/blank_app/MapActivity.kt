6c7b6a72c9394af43f565bc55bc89d5a1de269d4
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2023-12-08 20:45:11 -0500

    Simple marker for home screen

--- app/src/main/java/com/example/blank_app/MapActivity.kt
+++ app/src/main/java/com/example/blank_app/MapActivity.kt
@@ -3,12 +3,16 @@ package com.example.blank_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private var mGoogleMap:GoogleMap? = null
 
@@ -18,23 +22,31 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
 
-        //button
+        //button to exit map
         val MapBackButton = findViewById<Button>(R.id.MapBackButton)
 
-        //event listener
+        //event listener for exiting map
         MapBackButton.setOnClickListener{v ->
             finish()
         }
 
-        //map
+        //map fragment
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-
     }
 
-    //on map ready
+    //on map ready setup long click listener
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
+        mGoogleMap!!.setOnMapLongClickListener(this)
+        //default screen
+        val gvsu = LatLng(42.9636004, -85.8892062)
+        mGoogleMap!!.addMarker(MarkerOptions().position(gvsu))
+        mGoogleMap!!.moveCamera(CameraUpdateFactory.newLatLng(gvsu))
+    }
+
+    //what to do when long click
+    override fun onMapLongClick(latLng: LatLng) {
+        Log.d("DANS:", "lat is:" + latLng.toString())
     }
 }
