2a534f20e8b484956f62534374827915c0a4b45f
Brady Triola <131198404+GVKxngz@users.noreply.github.com>
2023-12-09 18:47:24 -0500

    pics

--- app/src/main/java/com/example/blank_app/MapActivity.kt
+++ app/src/main/java/com/example/blank_app/MapActivity.kt
@@ -1,5 +1,6 @@
 package com.example.blank_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -43,8 +44,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLong
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
-
 
         //button to exit map
         val mapBackButton = findViewById<Button>(R.id.MapBackButton)
